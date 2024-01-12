443932cb7f632ad0dd036c66664d185497ac7be5
Marta109 <marta.hayrapetyan.a@gmail.com>
2024-01-12 20:46:38 +0400

    feat: add solution getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -434,8 +434,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, i) => {
+    const arr = new Array(n).fill(0);
+    arr.splice(i, 1, 1);
+    return arr;
+  });
 }
 
 /**
